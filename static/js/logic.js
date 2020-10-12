bb8d97f2e85f3c3edfedc1ff45fafea5239a4557
Nate <nathanieldiamond97@gmail.com>
2020-10-12 07:58:40 -0600

    why didnt this commit last night

--- static/js/logic.js
+++ static/js/logic.js
@@ -23,3 +23,53 @@ function EQcolors(d){
     var depth_colors = ["#baffb2", "#fcf403", "#fe964c", "#fd763c", "#fc2a2a", "#f52f30", "#cf0000", "00dffc" ]
 
     console.log(EQcolors);
+
+    var depthMag = new L.LayerGroup(depthMag)
+
+function mapStyle(feature)
+{
+    return {
+        color: "#A9A9A9",
+        fillColor: EQcolors(feature.geometry.coordinates[2]),
+        fillOpacity: 0.75,
+        radius: feature.properties.mag*5 
+    }
+    }
+    
+    d3.json(geodata, function(data){
+    createFeatures(data.features);
+    
+    L.geoJson(data, {
+        pointToLayer: function(feature, latlng){
+        return L.circleMarker(latlng)
+        
+        },
+        style: mapStyle
+    
+    
+    }).addTo(depthMag);
+    
+});
+function createFeatures(geodata){
+    var depth_array = [];
+    
+    var markers = L.markerClusterGroup();
+    var magnitude = [];
+    
+    //loop through data to make markers for each earthquake & collapse into marker clusters
+    for (var i = 0; i < geodata.length; i++){
+        var latlng = geodata[i].geometry.coordinates;
+        
+        var size = (geodata[i].properties.mag)*10000;
+        
+        depth_array.push(geodata[i].geometry.coordinates[2]);
+        
+    
+        //this adds a marker and builds a marker cluster group for each earthquake
+        var m = L.marker([latlng[1], latlng[0]], {title: "test"});
+        m.bindPopup("<h3>" + geodata[i].properties.place + "</h3><hr>" + "<h4> Magnitude: " + geodata[i].properties.mag + "</h4>")
+        markers.addLayer(m)
+    
+    
+    }};
+
