3e6862f55a062027717698b8c58b95352e177687
Nate <nathanieldiamond97@gmail.com>
2020-10-14 09:12:15 -0600

    couldnt get pirate map to work :( so i decided on satellite map. that works tho

--- static/js/logic.js
+++ static/js/logic.js
@@ -115,16 +115,17 @@ function createFeatures(geodata){
       accessToken: API_KEY
     });
   
-    var piratemap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-      maxZoom: 18,
-      id: "Pirates",
-      accessToken: API_KEY
-    });
+    var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "satellite-v9",
+        accessToken: API_KEY
+      });
+    
   
       var baseMaps = {
-        "Street Map": streetmap,
-        "Pirate Map": piratemap
+        "Street View": streetmap,
+        "Satellite View": satellite
       };
   
       
